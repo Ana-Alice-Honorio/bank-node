a254aadf140b5dab482a936d54ce01ffd0d5fa64
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2023-05-28 11:13:55 -0300

    start project

---
+++ ContaPoupança.js
@@ -0,0 +1,3 @@
+export class ContaPoupança {
+  constructor() {}
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import { Cliente } from "./Cliente";
+import { ContaCorrente } from "./ContaCorrente";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
+
+const cliente1 = new Cliente("");
+const cliente2 = new Cliente("");
+const cliente3 = new Cliente("");
